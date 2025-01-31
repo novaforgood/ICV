6f19eeaca7ae5d314c4eab0265f5104df77f0bd3
akhilesh <akhilesh.basetty@gmail.com>
2025-01-30 21:42:41 -0800

    update the clients apis for getting an individual client's info

--- icv/src/api/clients.ts
+++ icv/src/api/clients.ts
@@ -1,10 +1,28 @@
 import { db } from '@/data/firebase'
-import { Client } from '@/types/client'
-import { collection, getDocs } from 'firebase/firestore'
+import { ClientType } from '@/types/case-types'
+import { collection, getDocs, doc, getDoc, QueryDocumentSnapshot, updateDoc } from 'firebase/firestore'
 
-export async function getAllClients(): Promise<Client[]> {
+export async function getAllClients(): Promise<ClientType[]> {
     const clientsCollection = collection(db, 'clients')  // Ensure the collection name is correct
     const clientsSnapshot = await getDocs(clientsCollection)
-    const clientsList = clientsSnapshot.docs.map((doc) => doc.data() as Client)
+
+    const clientsList = clientsSnapshot.docs.map((doc) => ({
+        id: doc.id, // Firebase document ID
+        ...doc.data(), // Other client fields
+    })) as ClientType[]
+
     return clientsList
 }
+
+export async function getClientById(clientId: string): Promise<ClientType | null> {
+    const clientRef = doc(db, 'clients', clientId);
+    const clientSnap = await getDoc(clientRef);
+    
+    return clientSnap.exists() ? ({ id: clientSnap.id, ...clientSnap.data() } as ClientType) : null;
+
+}
+
+export const updateClient = async (id: string, updatedClientData: ClientType) => {
+  const clientRef = doc(db, "clients", id);
+  await updateDoc(clientRef, updatedClientData);
+};
