29acf7f0560055f425e528e1e455f8a72bb26bf2
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2025-03-04 20:59:19 -0800

    travis 👎

--- icv/src/app/_components/Navbar.tsx
+++ icv/src/app/_components/Navbar.tsx
@@ -3,21 +3,52 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { useUser } from '@/hooks/useUser'
 import { cn } from '@/lib/utils'
 import Symbol from '../../components/Symbol'
 import LogoutButton from './LogoutButton'
 
 const Navbar = () => {
+    // const [name, setName] = useState<string>('')
+    // const [email, setEmail] = useState<string>('')
+    // const [photoURL, setPhotoURL] = useState<string>('')
+    // // This useEffect hook listens for changes in the user's authentication state
+    // // When the user logs in or out, it updates the displayed name
+    // // The hook sets up a listener when the component mounts and cleans it up on unmount
+    // // The empty dependency array [] means this effect only runs once when mounted
+    // useEffect(() => {
+    //     //listen for changes in auth state
+    //     const unsubscribe = auth.onAuthStateChanged((user) => {
+    //         if (user) {
+    //             setName(user.displayName || '')
+    //             console.log('User display name:', user.displayName)
+    //             setEmail(user.email || '')
+    //             setPhotoURL(user.photoURL || '')
+    //             console.log('User photoURL:', user.photoURL)
+    //         } else {
+    //             setName('')
+    //             setEmail('')
+    //             setPhotoURL('')
+    //         }
+    //     })
+
+    //     return () => unsubscribe()
+    // }, [])
+
+    const { user } = useUser()
+
     return (
         <div className="fixed left-0 top-0 flex h-full w-64 flex-col items-center gap-4 bg-foreground text-background">
             <div className="flex w-full flex-col items-center justify-center gap-2 py-8">
                 <img
-                    src="https://picsum.photos/seed/meow/200"
+                    src={
+                        user?.photoURL || 'https://picsum.photos/seed/meow/200'
+                    }
                     alt="logo"
                     className="m-4 h-16 w-16 rounded-full"
                 />
-                <h1 className="text-xl font-bold">Akhilesh Basetty</h1>
-                <p>bakhilesh@gmail.com</p>
+                <h1 className="text-xl font-bold">{user?.displayName}</h1>
+                <p>{user?.email}</p>
             </div>
             <div className="flex w-full flex-col items-stretch justify-center">
                 <NavLink href="/">
