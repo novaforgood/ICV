107059576a22bfb5c58a9cd2aabc9cda028c3ecd
Sonav Agarwal <42980809+SonavAgarwal@users.noreply.github.com>
2025-03-11 17:43:55 -0700

    clean up search page

--- icv/src/app/(nav)/clients/page.tsx
+++ icv/src/app/(nav)/clients/page.tsx
@@ -5,10 +5,12 @@ import SearchComponent from '@/app/_components/SearchComponent'
 
 export default async function ClientsPage() {
     const clients = await getAllClients()
-    
+
     return (
-        <>
+        <div className="w-full p-6">
+            <h1 className="mb-4 mt-6 w-full text-6xl font-bold">Clients</h1>
+
             <SearchComponent clients={clients} />
-        </>
+        </div>
     )
 }
