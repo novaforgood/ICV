cce9b506e1b39906c53ae1069259f0c98af5022e
Travis Ha <travisha33@gmail.com>
2025-04-16 18:37:39 -0700

    final user collection function and working on layout for client profile

--- icv/src/app/(nav)/clients/[clientId]/layout.tsx
+++ icv/src/app/(nav)/clients/[clientId]/layout.tsx
@@ -1,50 +1,50 @@
-// 'use client'
+'use client'
 
-// import { useRouter } from "next/navigation"
-// import ProfileNav from "./components/profilenav"
-// import Image from 'next/image'
-// import { getClientById } from '@/api/make-cases/make-case'
+import { useRouter } from "next/navigation"
+import ProfileNav from "./components/profilenav"
+import Image from 'next/image'
+import { getClientById } from '@/api/make-cases/make-case'
 
-// export default async function layout({
-//     params,
-//     children,
-// }: Readonly<{
-//     params: {clientId: string}
-//     children: React.ReactNode
-// }>) {
-//     const router = useRouter()
-//     const { clientId } = params
-//     const client = await getClientById(clientId)
-//     return (
-//         <>
-// <div className="sticky left-0 top-0 flex w-full flex-col items-start rounded-md border-b bg-white">
-//             <div className="flex w-full flex-row px-10">
-//                 <div className="mr-8 pt-4">
-//                     <Image
-//                         src="/icv.png"
-//                         alt="ICV Logo"
-//                         width={150}
-//                         height={150}
-//                         priority
-//                         className="rounded-full"
-//                     />
-//                 </div>
-//                 <div className="sticky top-0 flex w-full flex-col pt-8">
-//                     <h1 className="text-5xl font-bold">
-//                         {client.firstName} {client.lastName}
-//                     </h1>
-//                     <p className="text-m pt-2 text-gray-600">
-//                         {client.clientCode}
-//                     </p>
-//                     <p className="text-m text-gray-600">
-//                         Homeless Department
-//                     </p>
-//                 </div>
-//             </div>
-//             <ProfileNav />
-//         </div>
-//          {/* Page content go here */}
-//       <main className="p-6">{children}</main>
-//         </>
-//     )
-// }
+export default async function layout({
+    params,
+    children,
+}: Readonly<{
+    params: {clientId: string}
+    children: React.ReactNode
+}>) {
+    const router = useRouter()
+    const { clientId } = params
+    const client = await getClientById(clientId)
+    return (
+        <>
+<div className="sticky left-0 top-0 flex w-full flex-col items-start rounded-md border-b bg-white">
+            <div className="flex w-full flex-row px-10">
+                <div className="mr-8 pt-4">
+                    <Image
+                        src="/icv.png"
+                        alt="ICV Logo"
+                        width={150}
+                        height={150}
+                        priority
+                        className="rounded-full"
+                    />
+                </div>
+                <div className="sticky top-0 flex w-full flex-col pt-8">
+                    <h1 className="text-5xl font-bold">
+                        {client.firstName} {client.lastName}
+                    </h1>
+                    <p className="text-m pt-2 text-gray-600">
+                        {client.clientCode}
+                    </p>
+                    <p className="text-m text-gray-600">
+                        Homeless Department
+                    </p>
+                </div>
+            </div>
+            <ProfileNav />
+        </div>
+         {/* Page content go here */}
+      <main className="p-6">{children}</main>
+        </>
+    )
+}
